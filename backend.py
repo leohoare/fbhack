41eae2f56c62b402c45bc641ea6ba219338ab107
leo <leo.hoare@live.com>
2019-04-27 13:30:34 +1000

    db structure

--- backend.py
+++ backend.py
@@ -3,8 +3,24 @@ from flask import Flask, abort, request
 from flask_restplus import Resource, Api, reqparse, fields
 from flask_sqlalchemy import SQLAlchemy
 
+import os
+
+
 app = Flask(__name__)
 api = Api(app, title='API', description='peer note API', default="Actions",  default_label=None)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] =  'sqlite:///' + os.path.join(basedir, 'database')
+db = SQLAlchemy(app)
+
+
+class Record(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # hash = db.Column(db.H)
+    text = db.Column(db.String(255), nullable = False)
+    transType = db.Column(db.Integer, nullable = False)
+    rank = db.Column(db.String(120), unique=True, nullable=False)
+    trans = db.Column(db.String(255), nullable=False)
+
 
 
 @api.route('/getTranslation')
@@ -15,14 +31,20 @@ class First(Resource):
         payload = request.get_json()
         text = payload["text"]
         translator = Translator()
-        transObj =translator.translate(text)
+        transObj = translator.translate(text)
+        records = Record.query.filter_by(text=text).all()
+        if not records:
+
+        print("record",record)
         # print(text)
         # parser = reqparse.RequestParser()
         # parser.add_argument('q')
         # args = parser.parse_args()
         return transObj.text, 200
 
+
 if __name__ == '__main__':
-    
+    db.create_all()
     # create_db('data.db')
     app.run(debug=True)
+    
